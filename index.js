b284eda853b1da2baa58a720eee076ec0ebae368
jittavee <vcopcom@gmail.com>
2025-07-03 10:05:36 +0700

    add api

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ app.get('/', (req,res) =>{
     res.send('Hello World! Railway API is running');
 })
 
+ app.get('/api/info', (req,res) => {
+    res.json({
+        message: 'This is a simple API running on Railway',
+        status: 'success',
+        timestamp: new Date().toISOString()
+    })
+ })
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
